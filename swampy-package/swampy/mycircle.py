c1e2519602d2b80435b7fc827d57fce35cc866fa
phillipsKP <kjphillips@suffolk.edu/>
2014-06-14 20:39:07 -0400

    still puzzled

--- swampy-package/swampy/mycircle.py
+++ swampy-package/swampy/mycircle.py
@@ -28,14 +28,52 @@ circle(bob,20)
 wait_for_user()
 """
 #from math import *
+bob.delay=0.01
 def circle(t,r):
 	#t = Turtle()
 	circum = 2 * pi * r
-	n = 50 
+	n = int(circum/3) +1
 	length = circum / n
 	polygon(t,length,n)		
-circle(bob,100)
+circle(bob,50)
+
+world.clear()
+"""
+def polyline(t, n, length, angle):
+	for i in range(n):
+		fd(t, length)
+		lt(t, angle)
+polyline(t,n,length,angle)
+
+def polygon(t, n, length):
+	angle = 360.0 / n
+	polyline(t, n, length, angle)
+"""
+def arc(t, r, theta):
+	arc_length = 2 * pi * r * theta / 360
+	n = int(arc_length / 3) + 1
+	step_length = arc_length / n
+	step_angle = float(theta) / n
+	for i in range(n):
+			fd(t, step_length)
+			lt(t, step_angle)
+arc(bob,50,60)
+
+#polyline(t, n, step_length, step_angle)
 
 
+"""
+def arc(t, r, angle):
+	arc_length = 2 * pi * r * angle / 360
+	n = int(arc_length / 3) + 1
+	step_length = arc_length / n
+	step_angle = float(angle) / n
+
+	for i in range(n):
+		fd(t, step_length)
+		lt(t, step_angle)
+
+arc(bob,20,30)
+"""
 wait_for_user()
 	
